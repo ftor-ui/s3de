5e872ea1fa63e7a18b50b44191e325a86a9dc34c
Ftor7 <vfndtq200433@yandex.ru>
2025-01-06 22:41:31 +0300

    Added GLAD library and modified CMakeLists.txt

--- src/main.c
+++ src/main.c
@@ -1,4 +1,49 @@
-int main(int const argc, char const **argv)
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main(void)
 {
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+    {
+        printf("Can't load GLAD!\n");
+    }
+
+    printf("OpenGL %d.%d", GLVersion.major, GLVersion.minor);
+
+    glClearColor(1, 1, 0, 1);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
